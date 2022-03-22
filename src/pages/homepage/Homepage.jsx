3f367f87984379daea66934da902647fa6b25292
ehsan <ehsansaraee6570@yahoo.com>
2022-03-22 13:24:47 +0430

    adding homepage

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,6 +1,15 @@
 import { Col, Row, Statistic, Typography } from 'antd';
+import { useGetCryptosQuery } from '../../services/cryptoApi';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Cryptocurrencies, News } from '../index';
 
 const Homepage = () => {
+   const { data, isFetching } = useGetCryptosQuery();
+   const globalStats = data?.data?.stats;
+
+   if (isFetching) return 'Loading...';
+
    return (
       <>
          <Typography.Title level={2} className="heading">
@@ -8,21 +17,54 @@ const Homepage = () => {
          </Typography.Title>
          <Row>
             <Col span={12}>
-               <Statistic title="Total Cryptocurrencies" value="5" />
+               <Statistic
+                  title="Total Cryptocurrencies"
+                  value={millify(globalStats.total)}
+               />
             </Col>
             <Col span={12}>
-               <Statistic title="Total Exchanges" value="5" />
+               <Statistic
+                  title="Total Exchanges"
+                  value={millify(globalStats.totalExchanges)}
+               />
             </Col>
             <Col span={12}>
-               <Statistic title="Total Market Cap" value="5" />
+               <Statistic
+                  title="Total Market Cap"
+                  value={millify(globalStats.totalMarketCap)}
+               />
             </Col>
             <Col span={12}>
-               <Statistic title="Total 24h Volume" value="5" />
+               <Statistic
+                  title="Total 24h Volume"
+                  value={millify(globalStats.total24hVolume)}
+               />
             </Col>
             <Col span={12}>
-               <Statistic title="Total Markets" value="5" />
+               <Statistic
+                  title="Total Markets"
+                  value={millify(globalStats.totalMarkets)}
+               />
             </Col>
          </Row>
+         <section className="home-heading-container">
+            <Typography.Title level={2} className="home-title">
+               Top 10 Cryptocurrencies in the world
+            </Typography.Title>
+            <Typography.Title level={3} className="show-more">
+               <Link to="/cryptocurrencies">Show More</Link>
+            </Typography.Title>
+         </section>
+         <Cryptocurrencies simplified />
+         <section className="home-heading-container">
+            <Typography.Title level={2} className="home-title">
+               Latest Crypto News
+            </Typography.Title>
+            <Typography.Title level={3} className="show-more">
+               <Link to="/news">Show More</Link>
+            </Typography.Title>
+         </section>
+         <News simplified />
       </>
    );
 };
